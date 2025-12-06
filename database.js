5f70ab0adf889f0bd9cd5f04d0c3228f2c23e254
webleak <therealcracky1010@gmail.com>
2025-12-06 19:32:58 -0300

    Create database.js

---
+++ database.js
@@ -0,0 +1,10 @@
+<script>
+        const firebaseConfig = {
+            apiKey: "AIzaSyBwj_as6pb5Bg81F9Cl9VWTKkekMM9Exfk",
+            authDomain: "pekora-forum.firebaseapp.com",
+            projectId: "pekora-forum",
+            storageBucket: "pekora-forum.firebasestorage.app",
+            messagingSenderId: "161910967859",
+            appId: "1:161910967859:web:6a28cecaae74b9b7e9dd46",
+            measurementId: "G-BPBTW5KBRK"
+        };
